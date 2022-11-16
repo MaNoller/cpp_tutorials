317d94ee3d8add9208ee0cba1327946f3775060f
Marcel Noller <96265697+MaNoller@users.noreply.github.com>
2022-11-16 23:16:02 +0100

    Create Output.cpp

---
+++ Output.cpp
@@ -0,0 +1,17 @@
+//Includes the iostream library
+#include <iostream>
+
+int main(){
+    std::cout << "Number1";
+    std::cout << "Number2" << std::endl;
+    std::cout << std::endl;         // endl für endline
+    /*Comment here!
+    Multiline!! YEAH*/
+    std::cout << "Number3" << std::endl;
+    std::cout << "Number4" << std::endl;
+
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+
+}
